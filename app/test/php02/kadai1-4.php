00fd4e4cfb6e4a2f4aec4f87c3f495c587a8f292
NobTak <nobu.endli.x@gmail.com>
2021-05-21 23:58:27 +0900

    php02 kadai1-5

---
+++ app/test/php02/kadai1-4.php
@@ -0,0 +1,40 @@
+<?php
+//課題１
+$a = 3;
+$b = 7;
+
+echo $a + $b;
+
+//改行
+echo "\n";
+
+
+//課題２
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月", ];
+
+echo $array_month[7];
+
+//改行
+echo "\n";
+
+
+//課題３
+$hello = "Hello, ";
+$name = "Takaoka";
+$world = "'s World!";
+
+echo $hello . $name . $world;
+
+//改行
+echo "\n";
+
+
+//課題4
+$tech_boost = "tech";
+
+echo $tech_boost .= "_boost";
+
+//改行
+echo "\n";
+
+?>
