8da06856b29a2c4c8516c93f781862039dd8e194
NobTak <nobu.endli.x@gmail.com>
2021-05-21 19:23:16 +0900

    first commit

---
+++ app/config/db.php
@@ -0,0 +1,5 @@
+<?php
+
+const DSN = "mysql:host=mysql;dbname=common;charaset=utf8";
+const USERNAME = "nobuhiro";
+const PASSWORD = "nobuhiro";
---
+++ app/view/todo/delete.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('./../../controller/TodoController.php');
+
+    $controller = new TodoController;
+    $result = $controller->delete();
+
+    $response = array();
+    if ($result) {
+        $response['result'] = "success";
+    } else {
+        $response['result'] = "fail";
+    }
+    
+    echo json_encode($response);
+?>
