4a7957dad69d1d0144d1e965bc22a60426751ffb
NobTak <nobu.endli.x@gmail.com>
2021-06-05 02:03:04 +0900

    laravel12 kadai3-8

--- app/techboost/mynews/resources/views/admin/profile/create.blade.php
+++ app/techboost/mynews/resources/views/admin/profile/create.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MyNews</title>
-</head>
-<body>
-    <h1>プロフィール作成画面</h1>
-</body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+ 
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
