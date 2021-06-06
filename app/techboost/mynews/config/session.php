9f21140ad2b2d7ca99c21abef2050eabf8fa1d3b
NobTak <nobu.endli.x@gmail.com>
2021-06-07 00:33:42 +0900

    laravel13 verup_php7.4+laravel7.0

--- app/techboost/mynews/config/session.php
+++ app/techboost/mynews/config/session.php
@@ -164,7 +164,8 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    //'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -192,6 +193,7 @@ return [
     |
     */
 
-    'same_site' => null,
+    //'same_site' => null,
+    'same_site' => 'lax',
 
 ];
