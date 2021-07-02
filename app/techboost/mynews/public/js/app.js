8d90e820b4009d508ed8baf4c59bf2ffb4c43e49
NobTak <nobu.endli.x@gmail.com>
2021-07-03 02:29:40 +0900

    xdebug setting

--- app/techboost/mynews/public/js/app.js
+++ app/techboost/mynews/public/js/app.js
@@ -1882,7 +1882,7 @@ __webpack_require__.r(__webpack_exports__);
  */
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
-window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js");
+window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js").default;
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
